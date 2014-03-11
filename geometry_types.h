0fb4b2c4842e11868b054fbf9040a677c40612c1
Francis Williams <francis@fwilliams.org>
2014-03-11 16:57:54 -0700

    Added geometry_types header

---
+++ geometry_types.h
@@ -0,0 +1,42 @@
+/*
+ * geometry_types.h
+ *
+ *  Created on: Mar 11, 2014
+ *      Author: francis
+ */
+
+#ifndef GEOMETRY_TYPES_H_
+#define GEOMETRY_TYPES_H_
+
+struct Sphere {
+	cl_float radius;
+	cl_float3 position;
+	cl_uint materialId;
+};
+
+struct Triangle {
+	cl_float3 v1, v2, v3;
+	cl_uint materialId;
+};
+
+struct PointLight {
+	cl_float3 position;
+	cl_float3 power;
+};
+
+struct Material {
+	cl_float3 reflection;
+	cl_float3 refraction;
+	cl_float3 diffuseColor;;
+};
+
+struct RenderParams {
+	cl_float maxRenderDist;
+	cl_uint numTris;
+	cl_uint numSpheres;
+	cl_uint numLights;
+};
+
+
+
+#endif /* GEOMETRY_TYPES_H_ */
