ceed4967dcb3cc678e4420037aa3931c6da2531b
Francis Williams <francis@fwilliams.org>
2014-03-18 17:52:45 -0700

    Adding appropriate header files

---
+++ cl_helpers.h
@@ -0,0 +1,177 @@
+/*
+ * cl_helpers.h
+ *
+ *  Created on: Mar 17, 2014
+ *      Author: francis
+ */
+
+#include "opencl_ide_fix.h"
+#include "cl_data_structures.h"
+
+#ifndef CL_HELPERS_H_
+#define CL_HELPERS_H_
+
+// TODO: Move all of these somewhere better
+#define NULL_TYPE_ID 0
+#define SPHERE_TYPE_ID 1
+#define TRIANGLE_TYPE_ID 2
+
+#define RAY_SURFACE_EPSILON 0.0001
+#define RAY_TRI_EPSILON 0.000001
+
+bool rayTriangle(struct Ray* ray, global struct Triangle* tri, float* outT) {
+	float3 e1, e2;
+	float3 P, Q, T;
+	float det, invDet, u, v;
+	float t;
+
+	//Find vectors for two edges sharing v1
+	e1 = tri->v2 - tri->v1;
+	e2 = tri->v3 - tri->v1;
+
+	//Begin calculating determinant - also used to calculate u parameter
+	P = cross(ray->direction, e2);
+	det = dot(e1, P);
+
+	//if determinant is near zero, ray lies in plane of triangle
+	if(det > -RAY_TRI_EPSILON && det < RAY_TRI_EPSILON) {
+		return false;
+	}
+
+	invDet = 1.0f / det;
+
+	//calculate distance from V1 to ray origin
+	T = ray->origin - tri->v1;
+
+	//Calculate u parameter and test bound
+	u = dot(T, P) * invDet;
+	if(u < 0.0f || u > 1.0f) {
+		//The intersection lies outside of the triangle
+		return false;
+	}
+
+	//Prepare to test v parameter
+	Q = cross(T, e1);
+
+	//Calculate v parameter and test bound
+	v = dot(ray->direction, Q) * invDet;
+	if(v < 0.0f || u + v  > 1.0f) {
+		//The intersection lies outside of the triangle
+		return false;
+	}
+
+	t = dot(e2, Q) * invDet;
+
+	if(t > RAY_TRI_EPSILON) {
+		*outT = t;
+		return true;
+	}
+
+	return false;
+}
+
+bool raySphere(struct Ray* r, global struct Sphere* s, float* t) {
+	float3 rayToCenter = s->position - r->origin ;
+	float dotProduct = dot(r->direction, rayToCenter);
+	float d = dotProduct*dotProduct - dot(rayToCenter,rayToCenter)+s->radius*s->radius;
+	float sqrtd = sqrt(d);
+
+	if(d < 0) {
+		return false;
+	}
+
+	*t = dotProduct - sqrtd;
+
+	if(*t < 0) {
+		*t = dotProduct + sqrtd;
+		if(*t < 0) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+float intersect(
+		struct Ray* ray,
+		global struct Sphere* spheres,
+		global struct Triangle* tris,
+		int* index, uint* type) {
+	float minT = MAX_RENDER_DISTANCE;
+
+	for(int i = 0; i < NUM_SPHERES; i++) {
+		float t;
+		if(raySphere(ray, &spheres[i], &t)) {
+			if(t < minT){
+				minT = t;
+				*type = SPHERE_TYPE_ID;
+				*index = i;
+			}
+		}
+	}
+
+	for(int i = 0; i < NUM_TRIANGLES; i++) {
+		float t;
+		if(rayTriangle(ray, &tris[i], &t)) {
+			if(t < minT){
+				minT = t;
+				*type = TRIANGLE_TYPE_ID;
+				*index = i;
+			}
+		}
+	}
+
+	return minT;
+}
+
+inline float dt(float3 v1, float3 v2) {
+	return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
+}
+
+
+float3 reflect(float3 v, float3 n) {
+	return v - 2.0f * dot(v, n) * n;
+}
+
+
+inline float3 computeRadiance(
+		float3* position, float3* normal,
+		global struct Material* material,
+		global struct PointLight* lights,
+		global struct Sphere* spheres,
+		global struct Triangle* tris) {
+
+	float3 color = (float3){0.0, 0.0, 0.0};
+
+	for(int j = 0; j < NUM_LIGHTS; j++) {
+		float3 L = lights[j].position - *position;
+		float3 pos = *position;
+		float distanceToLight = length(L);
+		L = normalize(L);
+
+		struct Ray shadowRay;
+		shadowRay.origin = *position + L*RAY_SURFACE_EPSILON;
+		shadowRay.direction = L;
+
+		int index;
+		uint type;
+		float st = intersect(&shadowRay, spheres, tris, &index, &type);
+
+		if(st > distanceToLight) {
+			#if defined CONST_BRDF
+			color += material->color*lights[j].power*max(0.0f, dot(*normal, L));
+
+			#elif defined BLINN_PHONG_BRDF
+			float3 H = normalize(L + -pos);
+			color += lights[j].power * (material->kd*max(0.0f, dot(*normal, L)) + material->ks*pow(max(0.0f, dot(*normal, H)), material->exp));
+			#elif defined PHONG_BRDF
+			color += lights[j].power * (material->kd*max(0.0f, dot(*normal, L)) + material->ks*pow(max(0.0f, dot(*normal, normalize(reflect(L,*normal)))), material->exp));
+			#elif defined COOK_TORRANCE_BRDF
+
+			#endif
+		}
+	}
+	return color;
+}
+
+#endif /* CL_HELPERS_H_ */
---
+++ multi_pass_renderer.h
@@ -0,0 +1,122 @@
+/*
+ * multi_pass_renderer.h
+ *
+ *  Created on: Mar 17, 2014
+ *      Author: francis
+ */
+
+#define __CL_ENABLE_EXCEPTIONS
+
+#include <CL/cl.hpp>
+#include <GL/gl.h>
+#include <memory>
+#include <vector>
+#include <string>
+
+#include "scene.h"
+
+#ifndef MULTI_PASS_RENDERER_H_
+#define MULTI_PASS_RENDERER_H_
+
+template <cl_device_type DEVICE_TYPE>
+struct MultiPassRenderer {
+	MultiPassRenderer(std::shared_ptr<Scene<DEVICE_TYPE>> scene,
+					  size_t vpWidth, size_t vpHeight,
+					  unsigned reflectivePasses, double maxRenderDistance);
+
+
+	void renderToTexture(GLuint tex, cl_float viewMat[16]);
+
+	void setScene(std::shared_ptr<Scene<DEVICE_TYPE>> newScene) {
+		scene = newScene;
+
+		// Create the program
+		std::map<std::string, std::string> defines {
+			{"NUM_SPHERES", std::to_string(scene->getNumSpheres())},
+			{"NUM_TRIANGLES", std::to_string(scene->getNumTriangles())},
+			{"NUM_LIGHTS", std::to_string(scene->getNumPointLights())},
+			{"NUM_MATERIALS", std::to_string(scene->getNumMaterials())},
+			{"MAX_RENDER_DISTANCE", std::to_string(100.0)}
+		};
+
+		cl::Program firstPassProg = scene->getCLDeviceContext()->createProgramFromFile(FIRST_PASS_FILE_NAME, defines);
+		firstPass = cl::KernelFunctor(cl::Kernel(firstPassProg, "first_pass"), deviceContext->commandQueue,
+				cl::NullRange, cl::NDRange(viewportWidth, viewportHeight),
+				cl::NullRange);
+
+		cl::Program reflectPassProg = scene->getCLDeviceContext()->createProgramFromFile(RFLCT_PASS_FILE_NAME, defines);
+		reflectPass = cl::KernelFunctor(cl::Kernel(reflectPassProg, "reflect_pass"), deviceContext->commandQueue,
+				cl::NullRange, cl::NDRange(viewportWidth, viewportHeight),
+				cl::NullRange);
+	}
+
+private:
+	const std::string FIRST_PASS_FILE_NAME = "first_pass.cl";
+	const std::string RFLCT_PASS_FILE_NAME = "reflect_pass.cl";
+
+	std::shared_ptr<Scene<CL_DEVICE_TYPE_GPU>> scene;
+	std::shared_ptr<ClDeviceContext<DEVICE_TYPE>> deviceContext;
+	unsigned numReflectivePasses;
+	double maxRenderDistance;
+
+	size_t viewportWidth, viewportHeight;
+	cl::KernelFunctor firstPass, reflectPass;
+	cl::Buffer params, rayBuffer, reflectivityBuffer;
+	cl::Buffer resImg;
+};
+
+template <cl_device_type DEVICE_TYPE>
+MultiPassRenderer<DEVICE_TYPE>::MultiPassRenderer(std::shared_ptr<Scene<DEVICE_TYPE>> scene,
+		size_t vpWidth, size_t vpHeight, unsigned numReflectivePasses, double maxRenderDistance) :
+				deviceContext(scene->getCLDeviceContext()), numReflectivePasses(numReflectivePasses),
+				maxRenderDistance(maxRenderDistance), viewportWidth(vpWidth), viewportHeight(vpHeight) {
+	setScene(scene);
+	this->rayBuffer = cl::Buffer(deviceContext->context, CL_MEM_READ_WRITE, sizeof(Ray)*viewportWidth*viewportHeight);
+	this->reflectivityBuffer = cl::Buffer(deviceContext->context, CL_MEM_READ_WRITE, sizeof(cl_float3)*viewportWidth*viewportHeight);
+	this->resImg = cl::Buffer(deviceContext->context, CL_MEM_READ_WRITE, sizeof(cl_float4)*viewportWidth*viewportHeight);
+}
+
+template <cl_device_type DEVICE_TYPE>
+void MultiPassRenderer<DEVICE_TYPE>::renderToTexture(GLuint tex, cl_float viewMat[16]) {
+	firstPass(rayBuffer,
+			reflectivityBuffer,
+			scene->getTriangleBuffer(),
+			scene->getSphereBuffer(),
+			scene->getPointLightBuffer(),
+			scene->getMaterialBuffer(),
+			resImg);
+
+	for(int i = 0; i < numReflectivePasses; ++i) {
+		reflectPass(rayBuffer,
+				reflectivityBuffer,
+				scene->getTriangleBuffer(),
+				scene->getSphereBuffer(),
+				scene->getPointLightBuffer(),
+				scene->getMaterialBuffer(),
+				resImg);
+	}
+
+	glBindTexture(GL_TEXTURE_2D, tex);
+
+	cl::size_t<3> origin;
+	origin.push_back(0);
+	origin.push_back(0);
+	origin.push_back(0);
+	cl::size_t<3> size;
+	size.push_back(viewportWidth);
+	size.push_back(viewportHeight);
+	size.push_back(1);
+
+	cl_float4 pixels[viewportWidth*viewportHeight];
+
+	deviceContext->commandQueue.enqueueReadBuffer(
+		resImg, true, 0, viewportWidth*viewportHeight*sizeof(cl_float4), pixels);
+
+	glTexImage2D(
+			GL_TEXTURE_2D,
+			0, GL_RGBA, viewportWidth, viewportHeight, 0, GL_RGBA, GL_FLOAT, pixels);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+#endif /* MULTI_PASS_RENDERER_H_ */
