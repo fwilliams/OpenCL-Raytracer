2dd40283947275033099748ecbd79d8b9c8e099f
Francis Williams <francis@fwilliams.org>
2014-04-04 17:57:42 -0700

    Tree works!

--- texture_atlas.h
+++ texture_atlas.h
@@ -6,6 +6,8 @@
  */
 
 #include <gli/gli.hpp>
+#include <glm/gtx/string_cast.hpp>
+
 #include <CL/cl.hpp>
 #include <vector>
 #include <map>
@@ -16,6 +18,125 @@
 #ifndef TEXTURE_ATLAS_H_
 #define TEXTURE_ATLAS_H_
 
+struct SpaceTree {
+	struct node {
+		static const unsigned NO_TEXTURE = -2;
+
+		node() : rect(glm::ivec4(0)), left(nullptr), right(nullptr), texId(NO_TEXTURE) {}
+		~node() {
+			if(left != nullptr) {
+				delete left;
+			}
+			if(right != nullptr) {
+				delete right;
+			}
+		}
+
+		glm::ivec4 rect;
+		node* left;
+		node* right;
+		int texId;
+
+		node* insert(unsigned tex, const glm::ivec2& size) {
+			std::cout << "Insert function!" << std::endl;
+
+			if(left != nullptr) {
+				std::cout << "Left tree ain't empty. Insert here!" << std::endl;
+				node* res = nullptr;
+				res = left->insert(tex, size);
+				if(res != nullptr) {
+					return res;
+				}
+			}
+			if(right != nullptr) {
+				std::cout << "Right tree ain't empty. Insert here!" << std::endl;
+				return right->insert(tex, size);
+			}
+
+			if(texId != NO_TEXTURE) {
+				std::cout << "There's already a texture here. fuckit.jpg!" << std::endl;
+				return nullptr;
+			}
+
+			if(rect.z < size.x || rect.w < size.y) {
+				std::cout << "This hole is too small. fuckit.jpg!" << std::endl;
+				return nullptr;
+			}
+
+			if(rect.z == size.x && rect.w == size.y) {
+				std::cout << "Perfect fit!. feelsgoodman!" << std::endl;
+				texId = tex;
+				return this;
+			}
+
+			std::cout << "Inserting: " << std::endl;
+			std::cout << "    " << glm::to_string(rect) << std::endl;
+			std::cout << "    " << glm::to_string(size) << std::endl;
+			left = new node();
+			right = new node();
+
+			unsigned dw = rect.z - size.x;
+			unsigned dh = rect.w - size.y;
+
+			if(dw > dh) {
+				std::cout << "dw > dh" << std::endl;
+				left->rect = glm::ivec4(rect.x, rect.y, size.x, rect.w);
+				right->rect = glm::ivec4(rect.x+size.x, rect.y, rect.z-size.x, rect.w);
+			} else {
+				std::cout << "dw <= dh" << std::endl;
+				left->rect = glm::ivec4(rect.x, rect.y, rect.z, size.y);
+				right->rect = glm::ivec4(rect.x, rect.y+size.y, rect.z, rect.w-size.y);
+			}
+
+			return left->insert(tex, size);
+		}
+
+	};
+
+	~SpaceTree() {
+		delete tree;
+	}
+
+	node* tree = nullptr;
+	glm::ivec4 insert(unsigned texId, glm::ivec2 size) {
+		if(tree == nullptr) {
+			std::cout << "Initialized dat new tree! " << glm::to_string(size) << std::endl;
+			tree = new node();
+			tree->rect = glm::ivec4(0, 0, size.x, size.y);
+		}
+
+		std::cout << "===== TREE HAS SIZE (" << tree->rect.z << ", " << tree->rect.w << ") =====" << std::endl;
+
+		node* newNode = tree->insert(texId, size);
+		if(newNode == nullptr) {
+			std::cout << "Didn't fit. Time to enlarge this tree!" << std::endl;
+			node* newRoot = new node();
+			if(tree->rect.z <= tree->rect.w) {
+				newRoot->rect = tree->rect;
+				newRoot->rect.z *= 2;
+
+				newRoot->left = tree;
+				newRoot->right = new node();
+				newRoot->right->rect = tree->rect;
+				newRoot->right->rect.x += tree->rect.z;
+				std::cout << "Make it wider! " << glm::to_string(newRoot->rect) << std::endl;
+			} else {
+				newRoot->rect = tree->rect;
+				newRoot->rect.w *= 2;
+
+				newRoot->left = tree;
+				newRoot->right = new node();
+				newRoot->right->rect = tree->rect;
+				newRoot->right->rect.y += tree->rect.w;
+				std::cout << "Make it longer! " << glm::to_string(newRoot->rect) << std::endl;
+			}
+			newNode = newRoot->right->insert(texId, size);
+			tree = newRoot;
+		}
+		return newNode->rect;
+	}
+
+};
 class TextureAtlas {
 public:
 	// Add a texture to the atlas and return a textureId
@@ -33,6 +154,7 @@ public:
 				devCtx.context, CL_MEM_READ_ONLY,
 				cl::ImageFormat(CL_RGBA, CL_UNORM_INT8), atlasSize.x, atlasSize.y);
 
+		std::cout << "atlas size: " << atlasSize.x << " " << atlasSize.y << std::endl;
 		unsigned index = 0;
 		for(auto i : rects) {
 			cl::size_t<3> origin;
@@ -44,6 +166,8 @@ public:
 			size.push_back(i.w);
 			size.push_back(1);
 
+			std::cout << "origin: " << origin[0] << " " << origin[1] << std::endl;
+			std::cout << "size: " << size[0] << " " << size[1] << std::endl;
 			devCtx.commandQueue.enqueueWriteImage(
 					textureAtlas, true, origin, size, 0, 0, textureData[index++].data());
 		}
@@ -51,17 +175,24 @@ public:
 		return textureAtlas;
 	}
 
+	unsigned clp2(unsigned x) {
+	   x = x - 1;
+	   x = x | (x >> 1);
+	   x = x | (x >> 2);
+	   x = x | (x >> 4);
+	   x = x | (x >> 8);
+	   x = x | (x >>16);
+	   return x + 1;
+	}
+
+
 	glm::ivec2 computePositions() {
-
-		struct node {
-			glm::ivec4 rect;
-			unsigned left, right;
-		};
-
 		std::vector<unsigned> ids;
 		for(unsigned i = 0; i < textureData.size(); i++) {
 			ids.push_back(i);
 		}
+
+		std::cout << "Number of IDs: " << ids.size() << std::endl;
 
 		std::sort(ids.begin(), ids.end(), [this](unsigned a, unsigned b){ return area(textureData[a]) > area(textureData[b]); } );
 
@@ -72,33 +203,13 @@ public:
 		unsigned dim = static_cast<unsigned>(glm::pow(2.0f, glm::ceil(glm::log(largestDim)/glm::log(2.0f))));
 
 		glm::ivec4 currentSize(0, 0, dim, dim);
-		unsigned scanlineHeight = height(*largestTexture);
 
+		SpaceTree tree;
 		for(auto i : ids) {
-			std::cout << i << std::endl;
-			if((currentSize.z - currentSize.x) < width(textureData[i])) {
-				if(currentSize.z == currentSize.w) {
-					currentSize.z *= 2;
-				} else {
-					currentSize.w *= 2;
-
-					currentSize.x = 0;
-					currentSize.y = currentSize.y+scanlineHeight;
-
-					scanlineHeight += height(textureData[i]);
-				}
-			}
-			rects[i] = glm::ivec4(currentSize.x, currentSize.y, width(textureData[i]), height(textureData[i]));
-
-			std::cout << "inserted texture " << i <<
-					" with size (" << width(textureData[i]) << ", " << height(textureData[i]) <<
-					") at position (" << currentSize.x << ", " << currentSize.y << ")." << std::endl;
-			std::cout << "atlas size is now (" << currentSize.z << ", " << currentSize.w << ")" << std::endl;
-
-			currentSize.x += width(textureData[i]);
+			std::cout << "ID: " << i << "-------------------------------------" << std::endl;
+			rects[i] = tree.insert(i, glm::ivec2(width(textureData[i]), height(textureData[i])));
 		}
-
-		return glm::ivec2(currentSize.z, currentSize.w);
+		return glm::ivec2(clp2(tree.tree->rect.z), clp2(tree.tree->rect.w));
 	}
 
 private:
