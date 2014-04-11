cf87267fce4845675a0b5e66f10f94cc4a23fc74
Francis Williams <francis@fwilliams.org>
2014-04-10 19:41:12 -0700

    I'm an idiot and didn't add files

---
+++ renderer/renderer.h
@@ -0,0 +1,161 @@
+/*
+ * multi_pass_renderer.h
+ *
+ *  Created on: Mar 17, 2014
+ *      Author: francis
+ */
+
+#define __CL_ENABLE_EXCEPTIONS
+
+#include <CL/cl.hpp>
+#include <GL/gl.h>
+#include <gli/gli.hpp>
+
+#include <memory>
+#include <vector>
+#include <string>
+
+#include "data_types.h"
+#include "texture_array.h"
+#include "detail/texture_atlas.h"
+
+#ifndef RENDERER_H_
+#define RENDERER_H_
+
+template <cl_device_type DEVICE_TYPE, BRDFType BRDF>
+struct Renderer {
+	Renderer(Scene<BRDF>& scene,
+			 TextureArray& textureArray,
+			 size_t vpWidth, size_t vpHeight,
+			 unsigned reflectivePasses, double maxRenderDistance) :
+					deviceContext(ClDeviceContext<DEVICE_TYPE>()), numReflectivePasses(reflectivePasses),
+					maxRenderDistance(maxRenderDistance), viewportWidth(vpWidth), viewportHeight(vpHeight) {
+
+		// Create the program
+		std::map<std::string, std::string> defines {
+			{"NUM_SPHERES", std::to_string(scene.spheres.size())},
+			{"NUM_TRIANGLES", std::to_string(scene.tris.size())},
+			{"NUM_LIGHTS", std::to_string(scene.lights.size())},
+			{"NUM_MATERIALS", std::to_string(scene.materials.size())},
+			{"MAX_RENDER_DISTANCE", std::to_string(maxRenderDistance)},
+			{std::string(Material<BRDF>::name()), std::string()}
+		};
+
+		cl::Program firstPassProg = deviceContext.createProgramFromFile(FIRST_PASS_FILE_NAME, defines, OPENCL_BASE_DIR);
+		firstPass = cl::KernelFunctor(cl::Kernel(firstPassProg, "first_pass"), deviceContext.commandQueue,
+				cl::NullRange, cl::NDRange(viewportWidth, viewportHeight),
+				cl::NullRange);
+
+		cl::Program reflectPassProg = deviceContext.createProgramFromFile(RFLCT_PASS_FILE_NAME, defines, OPENCL_BASE_DIR);
+		reflectPass = cl::KernelFunctor(cl::Kernel(reflectPassProg, "reflect_pass"), deviceContext.commandQueue,
+				cl::NullRange, cl::NDRange(viewportWidth, viewportHeight),
+				cl::NullRange);
+
+		texAtlas = TextureAtlas::makeTextureAtlas(deviceContext, textureArray);
+		packCLBuffers(scene);
+	}
+
+
+	void renderToTexture(GLuint tex, cl_float16 viewMatrix) {
+		firstPass(rayBuf,
+				reflectivityBuf,
+				triBuf,
+				sphereBuf,
+				lightBuf,
+				materialBuf,
+				texOffsetBuf,
+				texAtlas.data,
+				resImg,
+				viewMatrix);
+
+		for(int i = 0; i < numReflectivePasses; ++i) {
+			reflectPass(rayBuf,
+					reflectivityBuf,
+					triBuf,
+					sphereBuf,
+					lightBuf,
+					materialBuf,
+					texOffsetBuf,
+					texAtlas.data,
+					resImg);
+		}
+
+
+		cl_float4 pixels[viewportWidth*viewportHeight];
+
+		deviceContext.commandQueue.enqueueReadBuffer(
+			resImg, true, 0, viewportWidth*viewportHeight*sizeof(cl_float4), pixels);
+
+		glBindTexture(GL_TEXTURE_2D, tex);
+		glTexImage2D(
+				GL_TEXTURE_2D,
+				0, GL_RGBA, viewportWidth, viewportHeight, 0, GL_RGBA, GL_FLOAT, pixels);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+
+private:
+	const std::string FIRST_PASS_FILE_NAME = "first_pass.cl";
+	const std::string RFLCT_PASS_FILE_NAME = "reflect_pass.cl";
+	const std::string OPENCL_BASE_DIR = "renderer/detail/opencl";
+
+	ClDeviceContext<DEVICE_TYPE> deviceContext;
+	unsigned numReflectivePasses;
+	double maxRenderDistance;
+
+	size_t viewportWidth, viewportHeight;
+	cl::KernelFunctor firstPass, reflectPass;
+	cl::Buffer rayBuf, reflectivityBuf, resImg, triBuf, sphereBuf, lightBuf, materialBuf, texOffsetBuf;
+	TextureAtlas texAtlas;
+
+	inline void packCLBuffers(Scene<BRDF>& scene) {
+		rayBuf = cl::Buffer(deviceContext.context, CL_MEM_READ_WRITE, sizeof(Ray)*viewportWidth*viewportHeight);
+		reflectivityBuf = cl::Buffer(deviceContext.context, CL_MEM_READ_WRITE, sizeof(cl_float3)*viewportWidth*viewportHeight);
+		resImg = cl::Buffer(deviceContext.context, CL_MEM_READ_WRITE, sizeof(cl_float4)*viewportWidth*viewportHeight);
+
+		if(!scene.spheres.empty()) {
+			size_t sphereByteSize = sizeof(Sphere) * scene.spheres.size();
+			sphereBuf = cl::Buffer(deviceContext.context, CL_MEM_READ_ONLY, sphereByteSize);
+			deviceContext.commandQueue.enqueueWriteBuffer(
+					sphereBuf, true, 0, sphereByteSize, scene.spheres.data());
+		}
+
+		if(!scene.tris.empty()) {
+			size_t trisByteSize = sizeof(Triangle) *scene.tris.size();
+			triBuf = cl::Buffer(deviceContext.context, CL_MEM_READ_ONLY, trisByteSize);
+			deviceContext.commandQueue.enqueueWriteBuffer(
+					triBuf, true, 0, trisByteSize, scene.tris.data());
+		}
+
+		if(!scene.lights.empty()) {
+			size_t lightByteSize = sizeof(PointLight) * scene.lights.size();
+			lightBuf = cl::Buffer(deviceContext.context, CL_MEM_READ_ONLY, lightByteSize);
+			deviceContext.commandQueue.enqueueWriteBuffer(
+					lightBuf, true, 0, lightByteSize, scene.lights.data());
+		}
+
+		if(!scene.materials.empty()) {
+			size_t materialByteSize = sizeof(Material<BRDF>) * scene.materials.size();
+			materialBuf = cl::Buffer(deviceContext.context, CL_MEM_READ_ONLY, materialByteSize);
+			deviceContext.commandQueue.enqueueWriteBuffer(
+					materialBuf, true, 0, materialByteSize, scene.materials.data());
+		}
+
+		if(!texAtlas.texPositions.empty()) {
+			std::vector<cl_float4> textureOffsets;
+			std::for_each(texAtlas.texPositions.begin(), texAtlas.texPositions.end(),
+					[&](glm::ivec4& rect) {
+						cl_float4 frect = cl_float4{
+							{static_cast<float>(rect.x), static_cast<float>(rect.y),
+							 static_cast<float>(rect.z), static_cast<float>(rect.w)}};
+					textureOffsets.push_back(frect);
+			});
+
+			size_t texOffsetByteSize = sizeof(cl_float4) * textureOffsets.size();
+			texOffsetBuf = cl::Buffer(deviceContext.context, CL_MEM_READ_ONLY, texOffsetByteSize);
+			deviceContext.commandQueue.enqueueWriteBuffer(
+					texOffsetBuf, true, 0, texOffsetByteSize, textureOffsets.data());
+		}
+	}
+};
+
+#endif /* RENDERER_H_ */
---
+++ renderer/texture_array.h
@@ -0,0 +1,73 @@
+/*
+ * texture_array.h
+ *
+ *  Created on: Apr 7, 2014
+ *      Author: francis
+ */
+
+#include <gli/gli.hpp>
+
+#include <array>
+
+#ifndef TEXTURE_ARRAY_H_
+#define TEXTURE_ARRAY_H_
+
+typedef cl_uint TextureHdl;
+const cl_uint no_texture = 0;
+
+struct TextureArray {
+	typedef std::vector<gli::storage>::iterator iterator;
+
+	TextureHdl createTexture(gli::storage& strg) {
+		storage.push_back(strg);
+		return static_cast<TextureHdl>(storage.size());
+	}
+
+	template <size_t N>
+	std::array<TextureHdl, N> createTextures(std::array<gli::storage, N>& strg) {
+		std::array<TextureHdl, N> res;
+		for(unsigned i = 0; i < strg.size(); i++) {
+			res[i] = createTexture(strg[i]);
+		}
+
+		return res;
+	}
+
+	template <size_t N>
+	std::array<TextureHdl, N> createTextures(std::initializer_list<gli::storage> strg) {
+		std::array<TextureHdl, N> res;
+		unsigned index = 0;
+		for(auto i : strg) {
+			res[index++] = createTexture(i);
+		}
+
+		return res;
+	}
+
+	gli::storage& operator[](TextureHdl hdl) {
+		return storage[hdl - 1];
+	}
+
+	size_t size() const {
+		return storage.size();
+	}
+
+	iterator begin() {
+		return storage.begin();
+	}
+
+	iterator end() {
+		return storage.end();
+	}
+
+	bool empty() const {
+		return storage.empty();
+	}
+
+private:
+	std::vector<gli::storage> storage;
+};
+
+
+
+#endif /* TEXTURE_ARRAY_H_ */
